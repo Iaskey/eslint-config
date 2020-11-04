c43d15f9cf9299d210b0fe598d17c41a0620c67e
Isaac Askey <iaskey@huskers.unl.edu>
2020-11-04 08:28:07 -0600

    deleted redundant files not used for linting

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
   "extends": "./base",
   "rules": {
-    "no-magic-numbers": ["warn", {
-      "ignore": [-1, 0, 0.001, 0.5, 1, 2, 10, 100, 180, 360, 1000, "-1n", "0n", "1n", "2n", "4n"],
+    "no-magic-numbers": ["off", {
+      "ignore": [-1, 0, 0.001, 0.5, 1, 2, 10, 100, 180, 360, 1000],
       "detectObjects": true,
     }],
   },
--- react.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "extends": "./base",
-  "rules": {
-    "no-magic-numbers": ["off", {
-      "ignore": [-1, 0, 0.001, 0.5, 1, 2, 10, 100, 180, 360, 1000],
-      "detectObjects": true,
-    }],
-  },
-}
